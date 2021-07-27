99344403727cda050cfbf74d3e3b201361dc472a
Alejandro Zamudio Guajardo <alex.zamudio88@gmail.com>
2021-07-27 17:59:25 +0200

    Creating iOS app project.

---
+++ Balam/BalamApp.swift
@@ -0,0 +1,17 @@
+//
+//  BalamApp.swift
+//  Balam
+//
+//  Created by Alejandro Zamudio Guajardo on 27/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct BalamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Balam/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Balam
+//
+//  Created by Alejandro Zamudio Guajardo on 27/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
